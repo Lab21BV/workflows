f69d43056867c9f96199fba4bb9e79ccae2cad01
Victor <victor@xcxinternational.com>
2026-05-24 13:04:35 +0200

    Register vi-reschedule workflow

--- src/workflows/registry.ts
+++ src/workflows/registry.ts
@@ -3,12 +3,14 @@ import { voorinspectieAfgerond } from "./voorinspectie-afgerond";
 import { voorinspectieNoResponse } from "./voorinspectie-no-response";
 import { showroomAfspraakGeweest } from "./showroom-afspraak-geweest";
 import { klantenserviceNieuw } from "./klantenservice-nieuw";
+import { viReschedule } from "./vi-reschedule/run";
 
 export const WORKFLOWS: Record<string, Workflow<unknown>> = {
   [voorinspectieAfgerond.id]: voorinspectieAfgerond as Workflow<unknown>,
   [voorinspectieNoResponse.id]: voorinspectieNoResponse as Workflow<unknown>,
   [showroomAfspraakGeweest.id]: showroomAfspraakGeweest as Workflow<unknown>,
   [klantenserviceNieuw.id]: klantenserviceNieuw as Workflow<unknown>,
+  [viReschedule.id]: viReschedule as Workflow<unknown>,
 };
 
 export type WorkflowId = keyof typeof WORKFLOWS;
